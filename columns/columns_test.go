875de81dc11f1a23b87bc51f45f575388b2d83c4
Arteev Aleksey <alarteev@yandex.ru>
2018-06-11 21:15:33 +0500

    columns:added unit tests

--- columns/columns_test.go
+++ columns/columns_test.go
@@ -1,8 +1,46 @@
 package columns
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 )
+
+func TestVisit(t *testing.T) {
+	var columns Columns
+	c1, err := columns.NewColumn("Col1", "Columns 1", 10, AlignLeft)
+	if err != nil {
+		t.Error(err)
+	}
+	c2, err := columns.NewColumn("Col2", "Columns 2", 10, AlignLeft)
+	if err != nil {
+		t.Error(err)
+	}
+	countVisit := 0
+	err = columns.Visit(func(c *Column) error {
+		if c1 != c && countVisit == 0 {
+			t.Errorf("Expected %v , got %v", c1, c)
+		}
+		if c2 != c && countVisit == 1 {
+			t.Errorf("Expected %v , got %v", c2, c)
+		}
+		countVisit++
+		return nil
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if countVisit != 2 {
+		t.Errorf("Expected visit %d,actual %d", 2, countVisit)
+	}
+
+	err = columns.Visit(func(c *Column) error {
+		return errors.New("test")
+	})
+	if err == nil {
+		t.Error("Expected error")
+	}
+}
 
 func TestColumnsVisible(t *testing.T) {
 	var columns Columns
@@ -94,4 +132,59 @@ func TestColumns(t *testing.T) {
 		t.Errorf("Excepted %v, got %v", ErrorAlreadyExists, err)
 	}
 
+	got := columns.Get(0)
+	if got != col1 {
+		t.Errorf("Exptected %v,got %v", col1, got)
+	}
+
+}
+
+func TestColumn(t *testing.T) {
+	var columns Columns
+	col, err := columns.NewColumn("Col1", "Columns 1", 10, AlignLeft)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := col.GetWidth()
+	if got != 10 {
+		t.Errorf("Expected %d,got %d", 10, got)
+	}
+
+	col.MaxLen = 15
+	got = col.GetWidth()
+	if got != col.MaxLen {
+		t.Errorf("Expected %d,got %d", col.MaxLen, got)
+	}
+
+	mask := "%-15v"
+	if got := col.GetMaskFormat(); got != mask {
+		t.Errorf("Expected %s,got %s", mask, got)
+	}
+
+	col.Aling = AlignRight
+	mask = "%15v"
+	if got := col.GetMaskFormat(); got != mask {
+		t.Errorf("Expected %s,got %s", mask, got)
+	}
+}
+
+func TestGetColumns(t *testing.T) {
+	var columns Columns
+	_, err := columns.NewColumn("Col1", "Columns 1", 10, AlignLeft)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = columns.NewColumn("Col2", "Columns 2", 15, AlignLeft)
+	if err != nil {
+		t.Fatal(err)
+	}
+	col2 := columns.Columns()
+	if len(col2) != len(columns.columns) {
+		t.Fatalf("Expected count columns %d,got %d", len(columns.columns), len(col2))
+	}
+	for i := 0; i < len(col2); i++ {
+		if !reflect.DeepEqual(col2[i], *columns.columns[i]) {
+			t.Errorf("Expected %v,got %v", *columns.columns[i], col2[i])
+		}
+	}
 }
